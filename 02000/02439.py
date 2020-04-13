6d6284f1e38905e83d546a5e6e0b1a815143a546
Sondho <thseogh135@gmail.com>
2020-04-13 20:44:19 +0900

    200413 풀이

---
+++ 02000/02439.py
@@ -0,0 +1,12 @@
+width = int(input())
+for i in range(0, width):
+    print(("*"*(i+1)).rjust(width))
+
+
+# width = int(input())
+# for i in range(0, r):
+#     print("{0:>{1}}".format("*"*(i+1), width))
+
+# x = int(input())
+# for i in range(1, x+1):
+#     print(' '*(x-i)+'*'*i)
