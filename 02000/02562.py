69302f5024ab34ef454bf7c245c030c2a71e75be
Sondho <thseogh135@gmail.com>
2020-05-04 23:49:28 +0900

    2020.05.04

--- 02000/02562.py
+++ 02000/02562.py
@@ -1,10 +1,15 @@
-number = []
-for i in range(9):
-    number.append(int(input()))
+number = [int(input()) for i in range(9)]
 
-max_number = number[0]
-for i in number:
-    if max_number < i:
-        max_number = i
-print(max_number)
-print(number.index(max_number)+1)
+print(max(number))
+print(number.index(max(number))+1)
+
+# number = []
+# for i in range(9):
+#     number.append(int(input()))
+
+# max_number = number[0]
+# for i in number:
+#     if max_number < i:
+#         max_number = i
+# print(max_number)
+# print(number.index(max_number)+1)
