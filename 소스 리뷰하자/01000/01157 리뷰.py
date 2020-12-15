5e8de1ecb14977b1e68a818ba4896f1e84fbc34c
Sondho <thseogh135@gmail.com>
2020-12-15 17:21:05 +0900

    2020-12-15

--- 소스 리뷰하자/01000/01157 리뷰.py
+++
@@ -1,4 +0,0 @@
-s,a=input().lower(),[]
-for i in range(97,123):
- a.append(s.count(chr(i)))
-print('?'if a.count(max(a))>1 else chr(a.index(max(a))+97).upper())
--- 실패/01712.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-fixed_cost, variable_cost, product_price = map(int, sys.stdin.readline().split())
-break_even_point = -1
-if (product_price*1 - fixed_cost + (variable_cost*1)) < (product_price*2 - fixed_cost + (variable_cost*2)):
-    if fixed_cost/(1-variable_cost/product_price)/product_price > 0:
-        break_even_point = int(fixed_cost/(1-(variable_cost/product_price)))
-
-print(break_even_point)
