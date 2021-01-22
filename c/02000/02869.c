3273c3a722b7e9489f26f1326138bb65e9b81cfa
Sondho <thseogh135@gmail.com>
2021-01-22 19:43:35 +0900

    2021-01-22 Friday 19:43:35

---
+++ c/02000/02869.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int		main(void)
+{
+	int		A, B, V;
+
+	scanf("%d %d %d", &A, &B, &V);
+	if ((V - A) % (A - B) == 0)
+		printf("%d\n", (V - A) / (A - B) + 1);
+	else
+		printf("%d\n", (V - A) / (A - B) + 2);
+	return (0);
+}
